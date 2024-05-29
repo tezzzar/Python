0875b7391d64c552bec421c570743ea415f0b678
tezzzar <olexandr.borodavko@gmail.com>
2024-05-29 13:21:52 +0300

    task 4 done

--- lecture-02/lesson_02_(01-06).py
+++ lecture-02/lesson_02_(01-06).py
@@ -35,5 +35,20 @@ if pal(string):
     print("The entered string is palindrome")
 else:
     print("The entered string is not palindrome")
+print()
+# Завдання 4
+print("Завдання 4")
+str1 = "JhonDipPeta"
+str2 = "JaSonAy"
 
+def mid_chars(str1, str2):
+    """Ця функція отримує три середні символи"""
+    length1, length2 = len(str1), len(str2)
+    mid1 = length1 // 2
+    mid2 = length2 // 2
+    mid_char1 = str1[mid1 - 1:mid1 + 2]
+    mid_char2 = str2[mid2 - 1:mid2 + 2]
+    return mid_char1, mid_char2
+
+print("Вихідні дані для Case 1, Case 2:", mid_chars(str1, str2))
 print()
