5e296bd156af201427837c5905374f18c7d6f3f9
tezzzar <olexandr.borodavko@gmail.com>
2024-06-04 13:21:27 +0300

    Task 1 done

---
+++ lecture-03/lesson_03_(01-06).py
@@ -0,0 +1,11 @@
+# Завдання 1
+print("Завдання 1")
+def every_nth(lst, n):
+    return [lst[i] for i in range(n-1, len(lst), n)]
+result = every_nth([1, 2, 3, 4, 5, 6], 2)
+print(result)
+print()
+# Завдання 2
+print("Завдання 2")
+
+print()
